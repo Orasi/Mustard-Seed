a1649d5a4b6cbc939baf988dc8541972b7a65b37
Matt Watson <matt.watson@orasi.com>
2016-10-07 17:09:00 -0400

    more docs

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,5 +1,7 @@
 Apipie.configure do |config|
   config.app_name                = "Mustard"
+  config.app_info                = 'API documentation for the Mustard Results Server'
+  config.validate                = false
   config.api_base_url            = ""
   config.doc_base_url            = "/docs"
   # where is your API defined?
