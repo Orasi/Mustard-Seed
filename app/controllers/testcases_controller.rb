99bf2d8d2774bc6674698216b434daa830f88b8d
Matt Watson <matt.watson@orasi.com>
2016-11-10 08:07:57 -0500

    fix export

--- app/controllers/testcases_controller.rb
+++ app/controllers/testcases_controller.rb
@@ -100,48 +100,21 @@ class TestcasesController < ApplicationController
     @testcases = @project.testcases
     respond_to do |format|
       format.xlsx{
-        filename = 'Sometest.xlsx'
+
+        filename = "#{@project.name}-Testcases.xlsx"
         file_path =  Rails.root.join("downloads/reports/#{filename}")
-        p = Axlsx::Package.new
-        p.workbook do |wb|
-          styles = wb.styles
-          tbl_header  = styles.add_style :b => true, :alignment => { :horizontal => :center }
-          left_align  = styles.add_style alignment: {horizontal: :left}
-          wrap_text   = styles.add_style :alignment => {:wrap_text => true}
 
-
-          wb.add_worksheet do |sheet|
-            sheet.name = 'Test Cases'
-            sheet.add_row ["Test ID", 'Title', 'Steps', 'Expected Result'], :style => tbl_header
-            @testcases.each do |tc|
-              steps = []
-              results = []
-              if tc.reproduction_steps
-                tc.reproduction_steps.each do |step|
-                  steps.append("#{step['step_number']}.  #{step['action']}")
-                  results.append("#{step['step_number']}.  #{step['result']}")
-                end
-              end
-              puts steps
-              sheet.add_row [tc.validation_id, tc.name, steps.join("\n"), results.join("\n")], style: [left_align, wrap_text, wrap_text, wrap_text]
-            end
-            sheet.column_widths 10, 30, 100, 100
-          end
-        end
-        p.use_shared_strings = true
-        p.serialize file_path
-        # render partial: 'export', locals: {filename: filename}
+        ExportTestcases.create(@testcases, file_path, filename)
         token = DownloadToken.create(expiration: DateTime.now + 30.seconds,
                                      path:  file_path,
                                      disposition: 'attachment',
                                      remove: true,
                                      content_type: 'application/octet-stream',
                                      filename: filename)
-        puts token.errors.full_messages
+
         render json: {report: download_url(token: token.token)}
-        # response.headers['Content-Disposition'] = 'attachment; filename="Testcases_Export.xlsx"'
       }
-      format.any{render json: @testcases}
+      format.any{ render json: @testcases }
     end
 
   end
