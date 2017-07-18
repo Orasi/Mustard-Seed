d7fd4e45696fc7be7e11717afe3408acfd743960
Charles Sexton <charlie_sexton@yahoo.com>
2017-07-18 09:26:32 -0400

    Added forgot password and forgot password form component.

---
+++ client/angular/src/app/components/forgot-password/forgot-password-form/forgot-password-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from "../../../services/user.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { emailValidator } from '../../../validators/validators';
+
+
+@Component({
+  selector: 'forgot-password-form',
+  templateUrl: './forgot-password-form.component.html'
+})
+export class ForgotPasswordFormComponent {
+
+  forgotPasswordForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private userService: UserService) {
+    this.forgotPasswordForm = fb.group({
+      'email': ['', Validators.compose([Validators.required,  emailValidator])]
+    });
+  }
+
+  resetPassword(values) {
+    this.userService.sendPasswordResetEmail(values.email);
+  }
+}
