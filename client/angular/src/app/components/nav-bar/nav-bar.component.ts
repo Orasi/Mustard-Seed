a0be55ff93724e0311ac3b5fcc038e885f5cc01a
Charles Sexton <charlie_sexton@yahoo.com>
2017-07-27 12:03:48 -0400

    Updated components, Nav-Bar, Login-Form, Forgot-Password and the dashboard

--- client/angular/src/app/components/nav-bar/nav-bar.component.ts
+++ client/angular/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectService } from '../../services/project.service';
+import { TeamService } from '../../services/team.service';
+
 
 @Component({
-  selector: 'app-nav-bar',
+  selector: 'nav-bar',
   templateUrl: './nav-bar.component.html'
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private projectService: ProjectService, private teamService: TeamService) { }
 
   ngOnInit() {
+    this.teamService.getTeam(1).subscribe(result => {
+        console.log(result);
+      },
+      err => {
+        console.log(err);
+      });
   }
-
 }
